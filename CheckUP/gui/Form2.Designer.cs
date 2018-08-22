8f681da0a5897648a4f1c39bd2b0cbc0ae00a63d
Ekapop <eploentham@gmail.com>
2018-08-22 18:58:45 +0700

    1
    1

--- CheckUP/gui/Form2.Designer.cs
+++ CheckUP/gui/Form2.Designer.cs
@@ -30,8 +30,10 @@
         {
             this.c1Button1 = new C1.Win.C1Input.C1Button();
             this.c1ThemeController1 = new C1.Win.C1Themes.C1ThemeController();
+            this.c1TextBox1 = new C1.Win.C1Input.C1TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.c1Button1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.c1ThemeController1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1TextBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // c1Button1
@@ -43,16 +45,26 @@
             this.c1Button1.Text = "c1Button1";
             this.c1Button1.UseVisualStyleBackColor = true;
             // 
+            // c1TextBox1
+            // 
+            this.c1TextBox1.Location = new System.Drawing.Point(329, 55);
+            this.c1TextBox1.Name = "c1TextBox1";
+            this.c1TextBox1.Size = new System.Drawing.Size(100, 18);
+            this.c1TextBox1.TabIndex = 1;
+            this.c1TextBox1.Tag = null;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.c1TextBox1);
             this.Controls.Add(this.c1Button1);
             this.Name = "Form2";
             this.Text = "Form2";
             ((System.ComponentModel.ISupportInitialize)(this.c1Button1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.c1ThemeController1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1TextBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -61,5 +73,6 @@
 
         private C1.Win.C1Input.C1Button c1Button1;
         private C1.Win.C1Themes.C1ThemeController c1ThemeController1;
+        private C1.Win.C1Input.C1TextBox c1TextBox1;
     }
 }
